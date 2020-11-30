1c427c351e30b5dc6791f4f9e7ca410075e6d2b9
Steven Yung <hyiuen@fastmail.com>
2020-11-30 23:22:32 +0100

    add deploy and fix postcss build issue

--- apps/website/node.api.js
+++ apps/website/node.api.js
@@ -1,14 +1,21 @@
 export default (pluginOptions) => ({
   webpack: (config, { defaultLoaders }) => {
+    const { cssLoader } = defaultLoaders;
+    for (const loader of cssLoader.loader) {
+      if (loader.loader === 'postcss-loader') {
+        loader.options.plugins = [
+          require('tailwindcss'),
+          require('autoprefixer'),
+        ];
+      }
+    }
+
     config.module.rules = [
       {
         oneOf: [
           defaultLoaders.jsLoader,
           defaultLoaders.jsLoaderExt,
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader', 'postcss-loader'],
-          },
+          cssLoader,
           defaultLoaders.fileLoader,
         ],
       },
